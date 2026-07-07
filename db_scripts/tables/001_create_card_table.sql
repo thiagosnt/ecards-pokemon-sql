f8b8463c4f4a6d193cd99883b63a4a2d4b198c6c
thiagosnt <150478228+thiagosnt@users.noreply.github.com>
2026-07-07 16:10:40 -0300

    001_create_card_table.sql

--- db_scripts/tables/001_create_card_table.sql
+++ db_scripts/tables/001_create_card_table.sql
@@ -1,1 +1,49 @@
+-- Table for collections
+CREATE TABLE tbl_collections (
+    id SERIAL PRIMARY KEY,
+    collectionSetName VARCHAR(100) NOT NULL,
+    releaseDate DATE NOT NULL,
+    totalCardsInCollection SMALLINT NOT NULL
+);
+
+-- Table for card types
+CREATE TABLE tbl_types (
+    id SERIAL PRIMARY KEY,
+    typeName VARCHAR(30) NOT NULL UNIQUE
+);
+
+-- Table for card stages
+CREATE TABLE tbl_stages (
+    id SERIAL PRIMARY KEY,
+    stageName VARCHAR(20) NOT NULL UNIQUE
+);
+
+-- Table for cards
+CREATE TABLE tbl_cards (
+    id SERIAL PRIMARY KEY,
+    hp SMALLINT,
+    name VARCHAR(80) NOT NULL,
+    type_id INT NOT NULL,
+    stage_id INT NOT NULL,
+    info TEXT,
+    attack VARCHAR(100),
+    dammage VARCHAR(20),
+    weak VARCHAR(30),
+    ressistance VARCHAR(30),
+    retreat VARCHAR(20),
+    cardNumberInCollection SMALLINT NOT NULL,
+    collection_id INT NOT NULL,
+    CONSTRAINT fk_collection
+        FOREIGN KEY (collection_id)
+        REFERENCES tbl_collections (id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_type
+        FOREIGN KEY (type_id)
+        REFERENCES tbl_types (id)
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_stage
+        FOREIGN KEY (stage_id)
+        REFERENCES tbl_stages (id)
+        ON DELETE RESTRICT
+);
 
