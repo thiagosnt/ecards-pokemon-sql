5d11731893a2ca54450728f370cb6e449b759691
thiagosnt <150478228+thiagosnt@users.noreply.github.com>
2026-07-07 16:56:49 -0300

    (opcional)
    migrar todos os arquivos de uma vez

--- db_scripts/seeds/to_migration.ps1
+++ db_scripts/seeds/to_migration.ps1
@@ -1,1 +1,22 @@
+#Pegar o diretório atual
+$scriptDirectory = Split-Path -PaTH $MyInvocation.MyCommand.Definition Definition -Parent
 
+#Arquivo saída com todos sql
+$outputFile = Join-Path -Path $scriptDirectory -ChildPath "migration.sql"
+
+#Verifica se arquivo já existe, se existir, deleta
+if (Test-Path $outputFile){
+    Remove-Item $outputFile
+    }
+
+#Pega Conteúdo dos arquivos
+$sqlFiles = Get-ChildItem -Path $scriptDirectory -Filter *.sql
+-File | Sort-Object Name
+
+#Concatena Arquivos
+foreach($file in $sqlFiles){
+Get-Content $file.FullName | Out-File -Append -FilePath $outputFile
+"Go" | Out-File -Append -FilePath $outputFile
+}
+
+Write-Host "Todos os arquivos foram combinados em $outputFile"
